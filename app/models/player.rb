d285ff9c53ce2183f1a4a47fd053b65f77c14183
Michael Choi <michaeljkchoi@gmail.com>
2014-12-10 17:46:00 -0500

    player validation tests

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,3 +1,3 @@
 class Player < ActiveRecord::Base
-  validates [:first_name, :last_name, :email], presence: true
+  validates :first_name, :last_name, :email, presence: true
 end
--- test/models/player_test.rb
+++ test/models/player_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class PlayerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should not save player without first name' do
+    player = Player.new(
+      last_name: players(:crosby).last_name,
+      number: players(:crosby).number,
+      email: players(:crosby).email,
+      phone_number: players(:crosby).phone_number
+    )
+    assert_not player.save, 'Saved player without first name'
+  end
+
+  test 'should not save player without last name' do
+    player = Player.new(
+      first_name: players(:crosby).first_name,
+      number: players(:crosby).number,
+      email: players(:crosby).email,
+      phone_number: players(:crosby).phone_number
+    )
+    assert_not player.save, 'Saved player without last name'
+  end
+
+  test 'should not save player without email' do
+    player = Player.new(
+      first_name: players(:crosby).first_name,
+      last_name: players(:crosby).last_name,
+      number: players(:crosby).number,
+      phone_number: players(:crosby).phone_number
+    )
+    assert_not player.save, 'Saved player without email'
+  end
 end
