cae213d55f440478c36c42dd2ec40e8d278f2867
Michael Choi <michaeljkchoi@gmail.com>
2014-12-29 01:00:08 -0500

    series controller tests

---
+++ test/controllers/series_controller_test.rb
@@ -0,0 +1,58 @@
+require 'test_helper'
+
+class SeriesControllerTest < ActionController::TestCase
+  setup do
+    @series = series(:twentyfourteen)
+  end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:series)
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should create series" do
+    assert_difference('Series.count') do
+      post :create, series: {
+        name: '2012 Series',
+        teams_attributes: {
+          team: { name: 'Boston Bruins' },
+          team: { name: 'Washington Capitals' }
+        }
+      }
+    end
+
+    assert_redirected_to series_path(assigns(:series))
+  end
+
+  test "should show series" do
+    get :show, id: @series
+    assert_response :success
+    assert_select 'h2', @series.name
+  end
+
+  test "should get edit" do
+    get :edit, id: @series
+    assert_response :success
+  end
+
+  test "should update series" do
+    patch :update, id: @series, series: {
+      name: '2011 Series'
+    }
+    assert_redirected_to series_path(assigns(:series))
+  end
+
+  test "should destroy series" do
+    assert_difference('Series.count', -1) do
+      delete :destroy, id: @series
+    end
+
+    assert_redirected_to series_index_path
+  end
+end
