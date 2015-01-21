f094f62ca0672834388952ffa109c049dc27ed6b
Michael Choi <michaeljkchoi@gmail.com>
2015-01-20 20:15:58 -0500

    player test cleanup

--- test/models/player_test.rb
+++ test/models/player_test.rb
@@ -1,27 +1,23 @@
 require 'test_helper'
 
 class PlayerTest < ActiveSupport::TestCase
-  test 'player saved with all parameters' do
+  test 'valid player' do
     player = Player.new(
       first_name: 'Michael',
       last_name: 'Choi',
       email: 'mchoi@beerleague.com'
     )
     assert player.save, 'Valid player was not saved'
-  end
-
-  test 'default reserve attribute is false' do
-    player = Player.create(
-      first_name: 'Michael',
-      last_name: 'Choi',
-      email: 'mchoi@beerleague.com'
-    )
     assert_equal false, player.reserve, 'Default reserve attribute is incorrect'
+    assert_equal 'Michael Choi', player.full_name
   end
 
-  test 'full name is correct' do
-    player = players(:crosby)
-    assert_equal 'Sidney Crosby', player.full_name, 'Full name method is incorrect'
+  test 'invalid player' do
+    player = Player.new()
+    assert_not player.save, 'Invalid player saved'
+    assert_equal "can't be blank", player.errors[:first_name].join(', '), 'No error for player without a first name'
+    assert_equal "can't be blank", player.errors[:last_name].join(', '), 'No error for player without a last name'
+    assert_equal "can't be blank", player.errors[:email].join(', '), 'No error for player without an email'
   end
 
   test 'slug is correct' do
@@ -52,20 +48,5 @@ class PlayerTest < ActiveSupport::TestCase
       email: players(:crosby).email
     )
     assert_not_equal players(:crosby).slug, impostor.slug, 'Slugs must be unique'
-  end
-
-  test 'player not saved without first name' do
-    player = Player.new(last_name: 'Choi', email: 'mchoi@beerleague.com')
-    assert_not player.save, 'Player without first name was saved'
-  end
-
-  test 'player not saved without last name' do
-    player = Player.new(first_name: 'Michael', email: 'mchoi@beerleague.com')
-    assert_not player.save, 'Player without last name was saved'
-  end
-
-  test 'should not save player without email' do
-    player = Player.new(first_name: 'Michael', last_name: 'Choi')
-    assert_not player.save, 'Player without email was saved'
   end
 end
